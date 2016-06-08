0b8c184a2a92bd4e52bc3937652fa48293d549d7
Tianlin Shi <tianlins@stanford.edu>
2016-06-07 17:06:34 -0700

    add exception handling to post handler

--- amabot/views.py
+++ amabot/views.py
@@ -1,5 +1,6 @@
 import json
 import sys
+import traceback
 from pprint import pprint
 sys.stdout = sys.stderr
 
@@ -26,28 +27,32 @@ class WebhookView(generic.View):
             return HttpResponse('Error, invalid token')
 
     def post(self, request, *args, **kwargs):
-        print '[post request]', id(self)
-        init_core_if_necessary()
-        # Converts the text payload into a python dictionary
-        incoming_message = json.loads(self.request.body.decode('utf-8'))
-        # Facebook recommends going through every entry since they might send
-        # multiple messages in a single call during high load
-        for entry in incoming_message['entry']:
-            for message in entry['messaging']:
-                # Check to make sure the received call is a message call
-                # This might be delivery, optin, postback for other events 
-                print '-----------------message-----------------'
-                pprint(message)
-                print '-----------------------------------------'
-                if 'postback' in message:
-                    handle_postback(message['sender']['id'], message['recipient']['id'], message['postback']['payload'])
-                elif 'message' in message:
-                    # Print the message to the terminal
-                    print '[posting message]'
-                    post_message(message['sender']['id'], message['recipient']['id'], message['message']['text'])
-                else: # start a new conversation, exciting!
-                    print '[adding user]'
-                    add_user(message['sender']['id'], message['recipient']['id'])
+        try:
+            print '[post request]', id(self)
+            init_core_if_necessary()
+            # Converts the text payload into a python dictionary
+            incoming_message = json.loads(self.request.body.decode('utf-8'))
+            # Facebook recommends going through every entry since they might send
+            # multiple messages in a single call during high load
+            for entry in incoming_message['entry']:
+                for message in entry['messaging']:
+                    # Check to make sure the received call is a message call
+                    # This might be delivery, optin, postback for other events 
+                    print '-----------------message-----------------'
+                    pprint(message)
+                    print '-----------------------------------------'
+                    if 'postback' in message:
+                        handle_postback(message['sender']['id'], message['recipient']['id'], message['postback']['payload'])
+                    elif 'message' in message:
+                        # Print the message to the terminal
+                        print '[posting message]'
+                        post_message(message['sender']['id'], message['recipient']['id'], message['message']['text'])
+                    else: # start a new conversation, exciting!
+                        print '[adding user]'
+                        add_user(message['sender']['id'], message['recipient']['id'])
+        except Exception as e:
+            print '[exception]', e.message
+            traceback.print_exc()
 
 
         return HttpResponse()
---
+++ scripts/bug_imposter.py
@@ -0,0 +1,5 @@
+from amabot.messenger import send_message
+
+imposter_id = '1033841673375305'
+
+send_message(imposter_id, '100002279386934', 'aloha')
